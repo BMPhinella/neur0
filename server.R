2ba09090976e30cac7011a26187185833ce7651d
BestMPhinella <40513508+BestMPhinella@users.noreply.github.com>
2018-07-09 18:27:07 +0300

    Add files via upload

--- server.R
+++ server.R
@@ -1,3 +1,62 @@
 library(shiny)
 
-shinyServer(function(input,output){}  )
+shinyServer(
+  
+  function(input,output){
+    
+    
+    
+    output$data<-renderTable({
+    if(is.null(input$file)){return()}
+      input$file #returns dataframe of the dataset
+    })
+    
+    
+    
+    output$sum <- renderPrint({
+      if(is.null(input$file)){return()}
+      summary(read.csv(file=input$file$datapath))
+    })
+    
+    
+  
+    
+    output$two<-renderPrint({
+      if(is.null(input$file)){return()}
+         str(input$file)
+    })
+    
+    
+    
+    output$onr<-renderPrint({
+      if(is.null(input$file)){return()}
+      input$file
+    })
+    
+    output$table<-renderUI({
+      if(is.null(input$file))
+        h4("good")
+      else
+        tabsetPanel(type="tab",
+                    tabPanel("Dataset ",tableOutput("data")),
+                    tabPanel("Data",tableOutput("onr")),
+                    tabPanel("Structure",tableOutput("two")),
+                    tabPanel("Summary",tableOutput("sum")),
+                    tabPanel("Plot",plotOutput("first")))
+      
+    })
+    
+    
+   
+    output$first<-renderPlot({
+      
+      bar1<- tapply(file[,input$yaxis], list(file[,input$xaxis]))
+      barplot(bar1)
+      
+      col<-as.numeric()
+      hist(dist [,col],main = "Data visuaization using a histogram")
+    })
+  }
+  
+  
+)
--- ui.R
+++ ui.R
@@ -1,39 +1,37 @@
 library(shiny)
-library(shinydashboard)
-shinyUI(
-  dashboardPage(
-    dashboardHeader(title = "iOS Mobile Application Analysis System",titleWidth = 700),
-                    
 
-    dashboardSidebar(
+shinyUI(fluidPage(
+  
+  titlePanel(title="iOS Mobile Application Analysis System"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
       
-      sidebarMenu(
-        menuItem("About", tabName = "dash1", icon = icon("tachometer")),
-        menuItem("Data", tabName = "dash2",icon = icon("tachometer")),
-        menuItem("Visualization", tabName = "dash3",icon = icon("tachometer")),
-        menuItem("Computation", tabName = "dash4",icon = icon("tachometer")),
-        menuItem("Help", tabName = "dash5",icon = icon("tachometer")),
-        menuItem("Feedback", tabName = "dash6"),
-        tags$style("body{background-color:purple;color:black}"),
-        br(),
-        downloadButton("file","Upload file")
-        
-      )
+      fileInput("file","Upload file"),
+     
+      uiOutput("Select file"),
+      
+      
+    selectInput("xaxis","Select the x axis",colnames(file)),
+                selectInput("yaxis","Select the y axis",colnames(file))
+
+     
       
       
     ),
     
-    dashboardBody("Graphical representation of the data")
-     
-   
-  
+    mainPanel(
+      uiOutput("table")
+      
+      
+      
+    )
     
-     )
+  )
   
   
-  
-)
-    
+))
  
   
   
