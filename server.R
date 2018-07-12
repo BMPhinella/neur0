5de5449bfc0cab719ae1461b30f63ee197c035b6
BestMPhinella <40513508+BestMPhinella@users.noreply.github.com>
2018-07-12 16:31:41 +0300

    Added a visualization functionality(bar plot)

--- server.R
+++ server.R
@@ -7,41 +7,35 @@ shinyServer(
     
     
     output$data<-renderTable({
-    if(is.null(input$file)){return()}
-      input$file #returns dataframe of the dataset
+    if(is.null(input$data_set)){return()}
+      input$data #returns dataframe of the dataset
     })
     
     
     
     output$sum <- renderPrint({
-      if(is.null(input$file)){return()}
-      summary(read.csv(file=input$file$datapath))
+      if(is.null(input$data_set)){return()}
+      summary(read.csv(data_set=input$data_set$datapath))
     })
     
     
   
     
     output$two<-renderPrint({
-      if(is.null(input$file)){return()}
-         str(input$file)
+      if(is.null(input$data_set)){return()}
+         str(input$data_set)
     })
     
     
     
     output$onr<-renderPrint({
-      if(is.null(input$file)){return()}
-      input$file
+      if(is.null(input$data_set)){return()}
+      input$data_set
     })
     
     
-    
-    output$abt<-renderPrint({
-    
-      input$at})
-    
-    
     output$table<-renderUI({
-      if(is.null(input$file))
+      if(is.null(input$data_set))
         h4("good")
       else
         tabsetPanel(type="tab",
@@ -55,10 +49,24 @@ shinyServer(
     
     
    
-    output$hist<-{(renderPlot(
-      
-    hist(mobile$fdd)  
-  ))}
+  output$hist<-renderPlot({
+    get_genre=input$genre
+    selected_data<-data_set%>%filter(prime_genre==get_genre)%>%select(rating_count_tot,user_rating,cont_rating,sup_devices.num,lang.num)
+    ggplot(selected_data,aes(x=user_rating))+geom_bar()
+    
+  })
    
+ 
+  
+  output$scat<-renderPlot({
+    get_genre2=input$genre2
+    selected_data2<-data_set%>%filter(prime_genre==get_genre2)%>%select(rating_count_tot,user_rating,
+                                                                      cont_rating,sup_devices.num,lang.num,ipadSc_urls.num,price,size_bytes)
+    ggplot(selected_data2, aes(x=user_rating, y==input$y)) + geom_point()
+  
+    
+  })
+  
+  
     
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,55 @@
 library(shiny)
 library(shinydashboard)
+library(dplyr)
+library(ggplot2)
+#adding data to the app
+data<-read.csv(file.choose(),header = T)
+data_set<-as.data.frame(data)
 
 ui<-fluidPage(
-  dashboardPage(skin = "purple",
-    dashboardHeader(title="iOS Mobile Application Analysis System"),
+  dashboardPage(skin = "red",
+    dashboardHeader(title="iOS Mobile Application Analysis System",
+                    
+                    dropdownMenu(type = "messages",
+                                 messageItem(
+                                   from = "About",
+                                   icon = icon("cog", lib = "glyphicon"),
+                                   message = "This system."
+                                 ),
+                                 messageItem(
+                                   from = "Help",
+                                   message = "How do I use this system?",
+                                   icon = icon("question"))),
+                           
+                                 dropdownMenu(type = "notifications",        
+                                    notificationItem(
+                                       text = "This is an iOs mobile application analysis system",
+                                       icon("users")
+                                     )
+                                     
+                                 )
+                    
+                    ),
     dashboardSidebar(
       
       sidebarMenu(
-        fileInput("file","Upload file"),
-        menuItem("About", tabName = "home"),
+   
+    br(),
         menuItem("Visualization", tabName = "visual",
+                 
                  menuSubItem("Histogram",tabName = "hist"),
                  menuSubItem("Barplot",tabName = "bar"),
                  menuSubItem("Scatter Plot",tabName = "scat")),
+    br(),
         menuItem("Computation", tabName = "comp",
                  menuSubItem("Total",tabName = "total"),
                  menuSubItem("Average",tabName = "average")
                  ),
+    br(),
         menuItem("Help", tabName = "help"),
         
-       
+    br(),
+    menuItem("About", tabName = "home"),
         
         uiOutput("Select file")
       )
@@ -31,29 +61,59 @@ ui<-fluidPage(
       tabItems(
         
         tabItem(tabName = "hist",fluidRow(
-          box(
+          box(title = "Barplot", background = "maroon", solidHeader = TRUE, height = "70%",
            
-            
-            textOutput("axis"),
+            selectInput("genre","Choose Genre",choices = c("Games","Finance","Productivity",
+                                                           "Reference","Music","Utilities","Travel",
+                                                           "Social Networking","Sports","Business",
+                                                            "Health & Fitness","Entertainment","Photo & Video",
+                                                           "Lifestyle","Food & Drink")),
+           # textOutput("axis"),
             plotOutput("hist"))
-      )
+      )),
       
-          
-         
-        )
+      
+    
         
+        tabItem(tabName = "home",fluidRow(
+          box(width = "30%" , height = "70%",
+            h4("This system is an iOS mobile application analysis system.
+               It analyses the IOS mobile application from apple store to have an insight
+               and determine the patterns from the Apple app store.
+               The insights enable in determinig :
+                 which apps are trending according to user rating.")
+            )
+        )),
+      
+      
+      
+      tabItem(tabName = "scat",fluidRow(
+        box(title = "Scatter plot", background = "blue", solidHeader = TRUE, height = "70%",
+            
+            selectInput("genre2","Choose Genre",choices = c("Games","Finance","Productivity",
+                                                           "Reference","Music","Utilities","Travel",
+                                                           "Social Networking","Sports","Business",
+                                                           "Health & Fitness","Entertainment","Photo & Video",
+                                                           "Lifestyle","Food & Drink")),
+            
+            selectInput("x","Choose the x axis",choices = c("rating_count_tot","user_rating","cont_rating","sup_devices.num","lang.num","ipadSc_urls.num","price","size_bytes")),
+            
+            selectInput("y","Choose the y axis",choices = c("rating_count_tot","user_rating","cont_rating","sup_devices.num","lang.num","ipadSc_urls.num","price","size_bytes")),
+            
+          
+            plotOutput("scat"))
+      ))
+      
       )
       
-     
       
-      
-    )
+    
     
      )
   
       
       
-    )
+    ))
     
 
       
