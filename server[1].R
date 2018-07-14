a4e1fb0c25e8a4c80ade3b6d382de4ff245a6aad
jeankeki <40161681+jeankeki@users.noreply.github.com>
2018-07-14 18:14:27 +0300

    Add files via upload

---
+++ server[1].R
@@ -0,0 +1,65 @@
+library(shiny)
+
+shinyServer(
+  
+  function(input,output){
+    
+    
+    
+    output$data<-renderTable({
+    if(is.null(input$file)){return()}
+      input$file #returns dataframe of the dataset
+    })
+    
+    
+    
+    output$sum <- renderPrint({
+      if(is.null(input$file)){return()}
+      summary(read.csv(file=input$file$datapath))
+    })
+    
+    
+  
+    
+    output$two<-renderPrint({
+      if(is.null(input$file)){return()}
+         str(input$file)
+    })
+    
+    
+    
+    output$onr<-renderPrint({
+      if(is.null(input$file)){return()}
+      input$file
+    })
+    
+    
+    
+    output$abt<-renderPrint({
+    if(is.null(input$home))
+      h1("This system is about")
+      })
+    
+    
+    output$table<-renderUI({
+      if(is.null(input$file))
+        h4("good")
+      else
+        tabsetPanel(type="tab",
+                    tabPanel("File property ",tableOutput("data")),
+                    tabPanel("Data",tableOutput("onr")),
+                    tabPanel("Structure",tableOutput("two")),
+                    tabPanel("File statistics",tableOutput("sum")),
+                    tabPanel("Plot",plotOutput("first")))
+      
+    })
+    
+    
+   
+    output$hist<-{(renderPlot(
+      
+    hist(mobile$fdd)  
+  ))}
+   
+    
+})
